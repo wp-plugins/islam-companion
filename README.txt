d13efabe5acabd574e188c3d8345d40e98984e69
nadirlatif <nadirlatif@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 13:40:52 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/islam-companion/tags/1.0.2@1065941 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,54 @@
+=== Islam Companion ===
+Contributors: nadirlatif
+Tags: islam,quran,sunnat,hadith,message of the day,motd
+Donate link: https://www.paypal.com/se/cgi-bin/webscr?cmd=_flow&SESSION=pOGdtsevzNpxZGAJ0cAH1htqgfrTsY_uociX-IkH0-QCBBAMUN38F8y1frK&dispatch=5885d80a13c0db1f8e263663d3faee8d66f31424b43e9a70645c907a6cbd8fb4
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0.2
+License: GPLV2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Islamic Companion is a Wordpress plugin that helps you integrate Islamic principals and values in your daily life
+
+== Description ==
+Islamic Companion allows users to learn about Islam and Quran from the comfort of their Wordpress blog. The goal of this plugin is to make it easy for people to learn about Islam and to spread its teachings to others. Currently the plugin has following features:
+
+1. Display verses from the Holy Quran in the language selected by the user. The message is displayed on the admin dashboard.
+
+New features will be added to the plugin. The user will have the option of enabling/disabling features. All plugin settings can be managed from the "Islam Companion" link under settings.
+
+== Installation ==
+Search for Islam Companion on https://wordpress.org/plugins/ or login to your wordpress blog and go to Plugins then Add New and then search for Islam Companion and Install. Another option is to install the plugin manually by following the instructions on http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation
+
+== Frequently Asked Questions ==
+1. What does this plugin do
+ans. Currently the Islam Companion plugin displays verses from the Holy Quran on the dashboard widget. The verse is displayed according to the user settings. The user can specify the language, narrator, suran, aya and number of verses.
+
+== Changelog ==
+
+= 1.0.2 =
+* Added option under settings for saving the surah, verse and verse count
+* Updated dashboard widget so it displays Quranic verses according to the settings
+
+= 1.0.1 =
+* Moved message for the day text to admin dashboard widget
+* Corrected bugs in displaying message for the day
+
+= 1.0.0 =
+* Added option for configuring language
+* Added function that displays Quranic verse at top of the admin page
+
+== Upgrade Notice ==
+
+= 1.0.2 =
+* Updated Holy Quran dashboard widget so it displays the verses according to the configured settings
+* Added option under settings for saving the surah, verse and verse count
+
+= 1.0.1 =
+* Message for the day text is now displayed in admin dashboard widget
+* Corrected bugs in displaying message for the day
+
+== Screenshots ==
+
+1. This screenshot shows the message for the day feature. A random verse from the Holy Quran is displayed on the admin dashboard. The verse changes after 1 day
+2. This screenshot shows the settings for the plugin. The language and translator for the Quranic verse can be configured here
---
+++ includes/plugins/class-message-for-the-day.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * The file that defines the "Message For The Day" feature
+ *
+ * A class definition that includes attributes and functions used on the admin dashboard.
+ *
+ * @link:       http://nadirlatif.me/islam-companion
+ * @since      1.0.0
+ *
+ * @package    Islam_Companion
+ * @subpackage Islam_Companion/includes
+ */
+
+/**
+ * The class for "Message for the day" feature.
+ *
+ * This is used to define internationalization, dashboard-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Islam_Companion
+ * @subpackage Islam_Companion/includes
+ * @author:       Nadir Latif <nadir@nadirlatif.me>
+ */
+class IC_MessageForTheDay {
+	/**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+    
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @var      string    $name       The name of the plugin.
+	 * @var      string    $version    The version of this plugin.
+	 */
+	public function __construct() {
+
+		// Set class property
+        $this->options = get_option( 'ic_options' );
+        
+	}
+
+	/**
+	 * Returns a random Quranic verse from the Holy Quran for the given language and translator
+	 *
+	 * @since    1.0.0
+	 */
+	private function DisplayVerseText() {
+		
+		global $wp_meta_boxes;
+
+		wp_add_dashboard_widget('message-for-the-day-widget', 'Holy Quran', array($this,'CustomDashBoardText'));
+		
+	}
+	
+	/**
+	 * Returns a random Quranic verse from the Holy Quran for the given language and translator
+	 *
+	 * @since    1.0.0
+	 */
+	public function CustomDashBoardText( $post, $callback_args ) {
+		
+		$encryption = new Encryption();
+		
+	    list($current_language,$current_narrator)=explode("~",$this->options['ic_narrator']);
+	    $current_language=$this->options['ic_language'];
+		list($current_sura,$ayat_count)=explode("~",$this->options['ic_sura']);
+		$current_aya=$this->options['ic_aya'];
+		$current_aya_count=$this->options['ic_ayat_count'];
+		
+		$verse_text=file_get_contents("http://nadirlatif.me/scripts/api.php?option=".urlencode(base64_encode("get_sura_verses"))."&lang=".urlencode(base64_encode($current_language))."&narrator=".urlencode(base64_encode($current_narrator))."&sura=".urlencode(base64_encode($current_sura))."&aya=".urlencode(base64_encode($current_aya))."&aya_count=".urlencode(base64_encode($current_aya_count)));
+		$verse_text=$encryption->DecryptText($verse_text);
+		$verse_text=wptexturize( $verse_text );
+		
+		$is_language_rtl=file_get_contents("http://nadirlatif.me/scripts/api.php?option=".urlencode(base64_encode("is_language_rtl"))."&lang=".urlencode(base64_encode($current_language)));
+		$is_language_rtl=trim($encryption->DecryptText($is_language_rtl));
+		
+		$rtl=($is_language_rtl=='true')?'right':'left';
+		$direction=($is_language_rtl=='true')?'rtl':'ltr';
+	
+		$ayat_text_str="";
+		$ayat_text_arr=explode("~",$verse_text);
+		
+		for($count=0;$count<count($ayat_text_arr);$count++)
+			{
+				if($rtl=="left")$ayat_text_str.="<li>".$ayat_text_arr[$count]."</li>";	
+				else $ayat_text_str.=$ayat_text_arr[$count]."</br>";
+			}
+			
+		if($rtl=="left")$ayat_text_str="<ol>".$ayat_text_str."</ol>";	
+		
+		if($rtl=="right")echo "<p style='direction:".$direction.";text-align:".$rtl.";word-wrap: break-word;font-size:120%;font-weight: bold;'>".$ayat_text_str."</p>";
+		else echo "<p style='direction:".$direction.";text-align:".$rtl.";word-wrap: break-word;'>".$ayat_text_str."</p>";
+		
+		//echo "<b><a href='#' onclick='FetchVerseData();'>&laquo; prev</a> | <a href='#' onclick='FetchVerseData();'>next &raquo;</a></b><br/>";
+		echo "<p><b>Surah: ".$current_sura." (verse ".$current_aya."-".($current_aya+$current_aya_count).")</b></p>";
+	}
+	
+	
+	/**
+	 * Function that need to be called in the admin_notices hook
+	 *
+	 * @since    1.0.0
+	 */
+	public function WPDashBoardSetupHook() {		
+		
+		$this->DisplayVerseText();
+		
+	}
+	
+	
+	/**
+	 * Function that is used to add a sub menu
+	 *
+	 * @since    1.0.0
+	 */
+	public function AddSubMenu() {		
+			
+		
+	}
+}
+?>
