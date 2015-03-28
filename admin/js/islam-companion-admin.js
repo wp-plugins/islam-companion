0633288a1263abcc89f439c693a87bfec3af66a4
nadirlatif <nadirlatif@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-28 20:29:50 +0000

    add missing files to repository
    git-svn-id: https://plugins.svn.wordpress.org/islam-companion/tags/1.0.5@1122849 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/js/islam-companion-admin.js
@@ -0,0 +1,122 @@
+/**
+	 * Checks if settings form contains valid data
+	 *
+	 * @since    1.0.0
+*/
+function ValidateICSettingsForm()
+	{
+		var ic_language_str=document.getElementById('ic_language').value;
+		var ic_narrator_str=document.getElementById('ic_narrator').value;
+		var ic_sura_str=document.getElementById('ic_sura').value;
+		var ic_ruku_str=document.getElementById('ic_ruku').value;
+		
+		if(ic_language_str==""){alert(objectL10n.language_alert);return false;}
+		if(ic_narrator_str==""){alert(objectL10n.narrator_alert);return false;}
+		if(ic_sura_str==""){alert(objectL10n.sura_alert);return false;}
+		if(ic_ruku_str==""){alert(objectL10n.ruku_alert);return false;}
+		return true;
+	}
+
+/**
+	 * Fetches verse data from the server	 
+	 *
+	 * @since    1.0.3
+*/
+function FetchVerseData(ajax_nonce,direction)
+	{		
+		var data = {
+					action: 'islam_companion',
+					plugin_action: 'fetch_verse_data',
+					security: ajax_nonce,
+					plugin: 'IC_HolyQuranDashboardWidget',
+					direction: direction
+		};
+		
+		// We can also pass the url value separately from ajaxurl for front end AJAX implementations
+		jQuery.post('admin-ajax.php', data, function(response) {
+			var result=jQuery.parseJSON(response);						
+			if(result.result=='success')jQuery("#ic-quran-dashboard-text").html(result.text);
+			else alert(objectL10n.data_fetch_alert);
+		});
+	}
+	
+(function( $ ) {
+	'use strict';
+
+	/**
+	 * All of the code for your Dashboard-specific JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note that this assume you're going to use jQuery, so it prepares
+	 * the $ function reference to be used within the scope of this
+	 * function.
+	 *
+	 * From here, you're able to define handlers for when the DOM is
+	 * ready:
+	 *
+	 * $(function() {
+	 *
+	 * });
+	 *
+	 * Or when the window is loaded:
+	 *
+	 * $( window ).load(function() {
+	 *
+	 * });
+	 *
+	 * ...and so on.
+	 *
+	 * Remember that ideally, we should not attach any more than a single DOM-ready or window-load handler
+	 * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
+	 * be doing this, we should try to minimize doing that in our own work.
+	 */
+
+	 $( window ).load(function() {		 
+		 
+		/**
+		 * Handles language dropdown click event
+		 *
+		 * @since    1.0.0
+		*/		 
+		 $("#ic_language").on("change",function(){
+			 	$('#ic_narrator').empty();
+			 	var ic_language_str=$("#ic_language").val();	  			
+	  			var ic_narrator_hidden_str=$("#ic_narrator_hidden").val();	  				  			
+	  			var temp_arr=ic_narrator_hidden_str.split("@");
+	  			
+	  			if(ic_language_str!="")$('#ic_narrator').append($('<option></option>').val("").html("--"+objectL10n.select_text+"--"));
+	  			else $('#ic_narrator').append($('<option></option>').val("").html("--"+objectL10n.language_select_text+"--"));
+	  			for(var count=0;count<temp_arr.length;count++)
+	  				{
+		  				var temp_arr1=temp_arr[count].split("~");
+		  				var temp_language=temp_arr1[0];
+		  				var temp_narrator=temp_arr1[1];
+		  				
+		  				if(ic_language_str==temp_language)
+		  					{			  					
+								$('#ic_narrator').append($('<option></option>').val(ic_language_str+"~"+temp_narrator).html(temp_narrator));											  			
+		  					}
+	  				}
+		 });
+		 
+		 /**
+		 * Handles sura dropdown click event
+		 *
+		 * @since    1.0.2
+		*/	
+		 $("#ic_sura").on("change",function(){
+			 	$('#ic_ruku').empty();
+			 	var ic_sura_str=$("#ic_sura").val();	  			
+			 	ic_sura_str=ic_sura_str.replace('\\','');	  			
+	  			var temp_arr=ic_sura_str.split("~");
+	  			var rukus=parseInt(temp_arr[2]);
+	  			
+	  			if(ic_sura_str!="")$('#ic_ruku').append($('<option></option>').val("").html("--"+objectL10n.select_text+"--"));
+	  			else $('#ic_sura').append($('<option></option>').val("").html("--"+objectL10n.sura_select_text+"--"));
+	  			for(var count=1;count<=rukus;count++)
+	  				{
+		  				$('#ic_ruku').append($('<option></option>').val(count).html(count));		  				
+	  				}
+		 });
+	 });
+})( jQuery );
---
+++ islam-companion.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * this starts the plugin.
+ *
+ * @link:       http://nadirlatif.me/islam-companion
+ * @since             1.0.0
+ * @package           Islam_Companion
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Islam Companion
+ * Plugin URI:        http://nadirlatif.me/islam-companion
+ * Description:        The goal of this plugin is to make it easier to integrate Islam in your every day life
+ * Version:           1.0.5
+ * Author:            Nadir Latif
+ * Author URI:        http://nadirlatif.me
+ * License:            GPL-2.0+
+ * License URI:        http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:            islam-companion
+ * Domain Path:            /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-islam-companion-activator.php';
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-islam-companion-deactivator.php';
+
+/** This action is documented in includes/class-islam-companion-activator.php */
+register_activation_hook( __FILE__, array( 'Islam_Companion_Activator', 'activate' ) );
+
+/** This action is documented in includes/class-islam-companion-deactivator.php */
+register_activation_hook( __FILE__, array( 'Islam_Companion_Deactivator', 'deactivate' ) );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-islam-companion.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_Islam_Companion() {
+
+	$plugin = new Islam_Companion();
+	$plugin->run();
+
+}
+run_Islam_Companion();
