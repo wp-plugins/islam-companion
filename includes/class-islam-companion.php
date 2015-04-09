09cc32656ff0744bd73e5c794a5126421a25e8b0
nadirlatif <nadirlatif@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-09 19:20:58 +0000

    added option for looking up the meaning of a word using an online dictionary
    git-svn-id: https://plugins.svn.wordpress.org/islam-companion/tags/1.0.6@1131415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-islam-companion.php
+++ includes/class-islam-companion.php
@@ -162,7 +162,7 @@ class Islam_Companion {
 		try
 			{
 				//$mail_from=ini_get("sendmail_from");
-				define("DEBUG",true);
+				define("DEBUG",false);
 				define("LOG_ERROR_EMAIL", 'nadir@nadirlatif.me');
 				define("LOG_ERROR_HEADER", "Subject: Error occured in Islam Companion Plugin. Please Check!\n");
 				define("LOG_FILE_NAME", "");
