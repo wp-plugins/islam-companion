bc31077c131e856227e3cedf85c6c99e5d29c394
nadirlatif <nadirlatif@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 19:56:21 +0000

    added option for selecting Holy Quran divisions on the settings page. updated the Holy Quran Dashboard widget layout
    git-svn-id: https://plugins.svn.wordpress.org/islam-companion/tags/1.1.0@1211090 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-islam-companion-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link:       http://pakjiddat.com
+ * @since      1.0.0
+ *
+ * @package    Islam_Companion
+ * @subpackage Islam_Companion/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Islam_Companion
+ * @subpackage Islam_Companion/includes
+ * @author:       Nadir Latif <nadir@pakjiddat.com>
+ */
+class Islam_Companion_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
